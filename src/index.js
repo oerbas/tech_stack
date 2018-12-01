95a4fc2a2453f63677212d066d7f9a07bd4c80b3
Orhan Erbas <orhanerba@icloud.com>
2018-12-02 00:05:31 +0300

    added common files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from "redux";
 import reducers from './reducers';
+import { Header } from "./components/common";
 
 class Index extends Component {
     render() {
         return (
             <Provider store={createStore(reducers)}>
                 <View>
+                    <Header headerText="Tech Stack"/>
                 </View>
             </Provider>
         )
